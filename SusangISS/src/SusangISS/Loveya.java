4bc6222dc8cd83cad68b6732be44bbd3d58cd933
data34 <data34@DESKTOP-NPBRAMR>
2021-03-17 09:30:52 +0900

    두번 째

---
+++ SusangISS/src/SusangISS/Loveya.java
@@ -0,0 +1,14 @@
+package SusangISS;
+
+public class Loveya {
+
+	public static void main(String[] args) {
+		System.out.println("Love Ya");
+		System.out.println("Happy");
+		System.out.println("LaLaLa");
+		
+		
+
+	}
+
+}
