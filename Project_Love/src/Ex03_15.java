06c8f81c15dcb94dbf3b39899dd7531aa19d1df3
Rimsswang <registerasp3@naver.com>
2021-03-24 09:43:22 +0900

    ha....

---
+++ Project_Love/src/Ex03_15.java
@@ -0,0 +1,23 @@
+
+public class Ex03_15 {
+
+	public static void main(String[] args) {
+		String str1 = "It CookBook 입니다.";
+		String str2 = "10";
+		String str3 = "20";
+		
+		System.out.println("str1 : " + str1);
+		str1 ="Java 입니다.";
+		
+		System.out.println("str1 : " + str1);
+		System.out.println("더하기 결과 : " + str2 + str3);
+		
+		int num1 = Integer.parseInt(str2);
+		int num2 = Integer.parseInt(str3);
+		System.out.println("더하기 결과 : " + (num1 + num2));
+		
+		System.out.printf("%s \n", str1);
+		System.out.printf("%s \n", str2 + str3);
+	}
+
+}
