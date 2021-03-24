ee0a2851d5afb90d468538bab2e100af7182cff3
Rimsswang <registerasp3@naver.com>
2021-03-24 11:06:06 +0900

    ㅁ

--- SusangISS/src/SusangISS/EX05_01.java
+++ SusangISS/src/SusangISS/EX05_01.java
@@ -3,11 +3,13 @@ package SusangISS;
 public class EX05_01 {
 
 	public static void main(String[] args) {
-		int a = 99;
+		int a = 100; 
 
-		if (a < 100)
-			System.out.printf("100보다 작군요..\n");
+		if (a < 100) 
+			System.out.println("100보다 작군요..");
+		System.out.println("나도 100보다 작은데..");
 
+		System.out.println("난 되지롱.");
 		
 	}
 
