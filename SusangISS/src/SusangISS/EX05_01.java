7e325333e183654c2aa2f2692b886238688c8f8f
Rimsswang <registerasp3@naver.com>
2021-03-30 08:56:13 +0900

    ㅁㄴㅇㄹ

--- SusangISS/src/SusangISS/EX05_01.java
+++ SusangISS/src/SusangISS/EX05_01.java
@@ -5,9 +5,9 @@ public class EX05_01 {
 	public static void main(String[] args) {
 		int a = 100; 
 
-		if (a < 100) 
+		if (a < 100) {
 			System.out.println("100보다 작군요..");
-		System.out.println("나도 100보다 작은데..");
+		System.out.println("나도 100보다 작은데.."); }
 		System.out.println("히히.");
 		
 		
