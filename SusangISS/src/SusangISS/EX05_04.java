0b81c229d9c4674bcc418dcad42e79cc327b9847
Rimsswang <registerasp3@naver.com>
2021-04-01 08:54:50 +0900

    haha

--- SusangISS/src/SusangISS/EX05_04.java
+++ SusangISS/src/SusangISS/EX05_04.java
@@ -5,10 +5,14 @@ public class EX05_04 {
 	public static void main(String[] args) {
 		int a=200;
 		
-		if (a<100)
-			System.out.printf("100보다 작군요..\n");
-		else
-			System.out.printf("100보다 크군요..\n");
+		if (a<100) {
+			System.out.println("100보다 작군요..");
+			System.out.println("참이면 보이져?");
+		} else {
+			System.out.println("100보다 크군요..");
+			System.out.println("거짓이면 보이죠?");
 	}
-
+		
+		  System.out.println("난 조건절에 상관없이 무조건 보이죠?");
+	}
 }
