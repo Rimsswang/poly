fcde89ee91281eee274c8f098c73ede0a857d5fb
Rimsswang <registerasp3@naver.com>
2021-04-06 08:49:52 +0900

    asdf

---
+++ SusangISS/src/SusangISS/Ex06_03.java
@@ -0,0 +1,20 @@
+package SusangISS;
+
+public class Ex06_03 {
+
+	public static void main(String[] args) {
+		int i;
+		for (i = 0; i < 3; i++) {
+			System.out.println("반복을 위한 i 값 : " + i);
+			System.out.println("또 안녕?");
+		}
+
+		System.out.println();
+		System.out.println();
+		for (i = 1; i <= 3; i++) {
+			System.out.println("반복 횟수 : " + i);
+		}
+
+	}
+
+}
