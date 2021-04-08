c13679637f4aab0065b0e74335f62bab2e3c3e92
Rimsswang <registerasp3@naver.com>
2021-04-08 20:01:06 +0900

    asdf

---
+++ SusangISS/src/SusangISS/Ex07_12.java
@@ -0,0 +1,27 @@
+package SusangISS;
+
+public class Ex07_12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hap = 0;
+		int i = 0;
+		while (true) {
+			// for(;;) {
+			for(i = 1; i <= 100; i++) {
+				hap += i;
+
+				if (hap > 2000) 
+				{
+					System.out.println("합계 : " + hap);
+					hap =0;
+					//break;
+					return;			
+				}
+			}
+			System.out.println("아직도 반복중...");
+		
+		}
+
+	}
+}
