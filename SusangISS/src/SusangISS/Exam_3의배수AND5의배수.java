f6d3bc210af343f297a7a01bbcd836ac2aff28f0
Rimsswang <registerasp3@naver.com>
2021-04-09 08:50:24 +0900

    asdf

---
+++ SusangISS/src/SusangISS/Exam_3의배수AND5의배수.java
@@ -0,0 +1,13 @@
+package SusangISS;
+
+public class Exam_3의배수AND5의배수 {
+
+	public static void main(String[] args) {
+		// 1부터 100까지 중에서 3의 배수와(AND) 5의 배수인 숫자만 출력하시오.
+		for (int i=1; i<=100; i++) {
+			if (i%3==0 && i%5==0)
+				{System.out.println("[3과 5의 배수]"+i);}
+		}
+	}
+
+}
