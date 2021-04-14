8f2944e76a94b9a0f927317c9a1e53015fbfdf21
Rimsswang <registerasp3@naver.com>
2021-04-14 09:28:20 +0900

    1234

--- SusangISS/src/SusangISS/Exam_While문_구구단.java
+++ SusangISS/src/SusangISS/Exam_While문_구구단.java
@@ -5,17 +5,15 @@ public class Exam_While문_구구단 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=2;
-		int j=1;
-
+		
 		while(i<10) {
-			j=1;
+			int j=1;
 			if(i==5 || i==7)
 			System.out.println(i+"[단] : ");
 			while(j<10) {
 			  System.out.println(i + "*" + j + "=" + (i*j));
 		      j++;
 			}
-			
 			System.out.println();
 			i++;
 			
