2c95a5c8f6028af624cd8a6ec1d3e5a0fa479b00
Rimsswang <registerasp3@naver.com>
2021-04-15 09:02:24 +0900

    asdf

---
+++ SusangISS/src/SusangISS/Ex06_14.java
@@ -0,0 +1,16 @@
+package SusangISS;
+
+public class Ex06_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  for (int i = 3; i<=8; i++) {
+			System.out.println("<"+i+">´Ü");
+			for(int k = 1; k<=9; k++) {
+				System.out.println(i+"x"+k+"="+(i*k));
+			}
+		}
+	}
+
+	
+}
