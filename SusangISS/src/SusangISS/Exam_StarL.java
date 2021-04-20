1718aeaa7b01a23811c737ed133754f5117aa59d
Rimsswang <registerasp3@naver.com>
2021-04-21 08:56:57 +0900

    asdf

---
+++ SusangISS/src/SusangISS/Exam_StarL.java
@@ -0,0 +1,26 @@
+package SusangISS;
+
+public class Exam_StarL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int b = 5;
+for ( int a=1; a<=9; a++) {
+	for(int c=1; c<=5; c++) {
+		if(c<b) {
+			System.out.print(" ");
+		}
+		else {
+			System.out.print("*");
+		}
+		}	
+	if(a<5) {
+		b--;
+	}else {
+		b++;
+	}
+System.out.println();
+}
+
+}
+}
