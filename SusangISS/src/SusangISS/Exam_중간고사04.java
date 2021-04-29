ba7ba518420fa14636d13200921aa5174c4af3d1
Rimsswang <registerasp3@naver.com>
2021-04-30 03:44:04 +0900

    asdfasdf

---
+++ SusangISS/src/SusangISS/Exam_중간고사04.java
@@ -0,0 +1,14 @@
+package SusangISS;
+
+public class Exam_중간고사04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=3; i<9; i++) {
+	for(int j=1; j<10; j++) {
+		System.out.println(i+"*"+j+"="+(i*j));
+	}
+}
+	}
+
+}
---
+++ SusangISS/src/SusangISS/Exam_중간고사05.java
@@ -0,0 +1,17 @@
+package SusangISS;
+
+public class Exam_중간고사05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+int hap=0;
+for(i=0; i<11; i++) {
+ if(i%4!=0) {
+	 hap+=i;
+}
+}
+System.out.println("출력결과 : "+hap);
+	}
+
+}
